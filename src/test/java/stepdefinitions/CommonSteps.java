b3da77b74aafb30356b2d7610d7b73bc99b5df2e
Vishalk86 <103045522+Vishalk86@users.noreply.github.com>
2026-07-30 18:40:55 +0530

    Update CommonSteps.java

--- src/test/java/stepdefinitions/CommonSteps.java
+++ src/test/java/stepdefinitions/CommonSteps.java
@@ -26,7 +26,7 @@ public class CommonSteps extends BaseClass
     public void launchBrowser() 
     {
         setup();
-        cf = new CommonFunctions(driver); // Testing July By Tester 6:30 PM
+        cf = new CommonFunctions(driver); // Testing July By Tester 6:30 PM, Now By Admin
     }
 
     @And("I open application")
